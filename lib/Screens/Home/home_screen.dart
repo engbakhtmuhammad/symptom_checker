8943cd2319455d0df7a12761c5aed05c104016c8
bakhtkhilji23 <bakhtkhilji23@gmail.com>
2021-04-25 03:28:37 +0500

    news section updated

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -36,91 +36,97 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => BioScreen())),
-            child: Container(
-              width: size.width,
-              height: size.height * .125,
-              decoration: BoxDecoration(
-                  color: kBackgroundColor,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(35),
-                      bottomRight: Radius.circular(35))),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 40, top: 10),
-                        child: Text(
-                          "How You Feel!",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
+    return SafeArea(
+        child: _loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => BioScreen())),
+                      child: Container(
+                        width: size.width,
+                        height: size.height * .125,
+                        decoration: BoxDecoration(
+                            color: kBackgroundColor,
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(35),
+                                bottomRight: Radius.circular(35))),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 40, top: 10),
+                                  child: Text(
+                                    "How You Feel!",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                ),
+                                SizedBox(
+                                    width: size.width / 1.5,
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        left: 40,
+                                      ),
+                                      child: Text(
+                                        "Tap here to check your symptom before it become worse",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15),
+                                      ),
+                                    ))
+                              ],
+                            ),
+                            Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                right: 30,
+                              ),
+                              child: Image.asset("assets/images/fever.png"),
+                            )
+                          ],
                         ),
+                        // child: RoundedButton(
+                        //     text: 'Check Symptoms',
+                        //     press: () {
+                        //       Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        //         return BioScreen();
+                        //       }));
+                        //     }),
                       ),
-                      SizedBox(
-                          width: size.width / 1.5,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 40,
-                            ),
-                            child: Text(
-                              "Tap here to check your symptom before it become worse",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 15),
-                            ),
-                          ))
-                    ],
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 30,
                     ),
-                    child: Image.asset("assets/images/fever.png"),
-                  )
-                ],
-              ),
-              // child: RoundedButton(
-              //     text: 'Check Symptoms',
-              //     press: () {
-              //       Navigator.push(context, MaterialPageRoute(builder: (context) {
-              //         return BioScreen();
-              //       }));
-              //     }),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 16),
-            child: ListView.builder(
-                itemCount: newslist.length,
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return NewsTile(
-                    imgUrl: newslist[index].urlToImage ?? "",
-                    title: newslist[index].title ?? "",
-                    desc: newslist[index].description ?? "",
-                    content: newslist[index].content ?? "",
-                    posturl: newslist[index].articleUrl ?? "",
-                  );
-                }),
-          ),
-        ],
-      ),
-    );
+                    Container(
+                      margin: EdgeInsets.only(top: 16),
+                      child: ListView.builder(
+                          itemCount: newslist.length,
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return NewsTile(
+                              imgUrl: newslist[index].urlToImage ?? "",
+                              title: newslist[index].title ?? "",
+                              desc: newslist[index].description ?? "",
+                              content: newslist[index].content ?? "",
+                              posturl: newslist[index].articleUrl ?? "",
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              ));
   }
 }
 
